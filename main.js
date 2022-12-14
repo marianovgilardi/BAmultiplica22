6bef642637131fcb759c061fe687ea6d939abfc9
Mariano Visco Gilardi <115675440+marianovgilardi@users.noreply.github.com>
2022-12-13 23:34:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+@import url('http://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+body {
+    font-family: 'Montserrat', sans-serif;
+    overflow-X: hidden;
+}
+
+
+#menu a {
+    font-size: 18px;
+    font-weight:bold;
+    color: #3f3f3f;
+}
+
+#menu button {
+    font-size: 18px;
+    font-weight:bold;
+}
